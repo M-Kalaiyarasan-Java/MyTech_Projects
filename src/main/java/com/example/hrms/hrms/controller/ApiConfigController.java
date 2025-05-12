adc047529a8c4925c5e7743320ef6d4bebabb305
Kalaiyarasan1619 <155789550+Kalaiyarasan1619@users.noreply.github.com>
2025-05-12 20:52:37 +0530

    api config solve and new jar added

--- src/main/java/com/example/hrms/hrms/controller/ApiConfigController.java
+++ src/main/java/com/example/hrms/hrms/controller/ApiConfigController.java
@@ -1,6 +1,5 @@
 package com.example.hrms.hrms.controller;
 
-import java.lang.classfile.ClassFile.Option;
 import java.util.List;
 import java.util.Optional;
 
@@ -11,13 +10,11 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.hrms.hrms.exception.ResourceNotFoundException;
 import com.example.hrms.hrms.model.ConfigTable;
-import com.example.hrms.hrms.model.Developers;
 import com.example.hrms.hrms.repo.ConFigApiRepo;
 
 @RestController
